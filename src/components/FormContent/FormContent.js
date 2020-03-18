996cbeede8e0f7454f100ef1d9e0e47360fbc2d0
Matthew Gramigna <mgramigna@mitre.org>
2020-03-18 12:42:57 -0400

    Added basic layout and top-level components
    * Use material ui for grid layout
    * Added 3 main top level components
    * Added basic tests for render call

---
+++ src/components/FormContent/FormContent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FormContent() {
+  return <div>Form content goes here</div>;
+}
+
+export default FormContent;
---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PageHeader() {
+  return <div>Page header goes here</div>;
+}
+
+export default PageHeader;
