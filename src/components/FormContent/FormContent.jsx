fcc947c90cb4bfb97fd22fd6c1505f8b8b778196
Noranda Brown <nebrown@mitre.org>
2020-03-19 14:55:04 -0400

    Updated styles

--- src/components/FormContent/FormContent.jsx
+++ src/components/FormContent/FormContent.jsx
@@ -1,22 +1,41 @@
 import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core';
+
 import { updateTOC, cleanUpTOC } from '../Navigation/Navigation';
 import FormSectionHeader from '../FormSectionHeader';
+
+const useStyles = makeStyles(
+  {
+    root: {
+      margin: '2em'
+    }
+  },
+  { name: 'FormContent' }
+);
 
 const sections = [
   { id: 'identifiers', title: 'identifiers' },
   { id: 'interviewer', title: 'interviewer' },
   { id: 'basicInformation', title: 'basic information' },
   { id: 'demographics', title: 'demographics' },
-  { id: 'diseaseInformation', title: 'disease information' }
+  { id: 'patientCare', title: 'patient care' },
+  { id: 'symptoms', title: 'symptoms' },
+  { id: 'exposure', title: 'exposure' }
 ];
 
 function FormContent() {
+  const styles = useStyles();
+
   useEffect(() => {
     updateTOC();
     return cleanUpTOC;
   });
 
-  return sections.map(s => <FormSectionHeader id={s.id} key={s.id} title={s.title} />);
+  return sections.map(s => (
+    <div className={styles.root}>
+      <FormSectionHeader id={s.id} key={s.id} title={s.title} />
+    </div>
+  ));
 }
 
 export default FormContent;
