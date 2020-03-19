b9bbb9a7c5b6dabf39dc49e4b9fef34d93bcb9bd
Rob Dingwell <rob@robdingwell.org>
2020-03-19 14:51:34 -0400

    initial cut at page header

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Image from './covid.png';
+const useStyles = makeStyles(theme => ({
+  bg: {
+    height: '170px',
+    backgroundImage: `url(${Image})`,
+    backgroundSize: 'cover'
+  },
+  text: {
+    textAlign: 'center left',
+    color: 'white',
+    fontFamily: "'Code-Pro-LC', sans-serif",
+    fontWeight: 600,
+    fontSize: '30px',
+    padding: '20px '
+  }
+}));
 
 function PageHeader() {
-  return <div>Page header goes here</div>;
+  const classes = useStyles();
+  return (
+    <Grid container xs={12} className={classes.bg}>
+      <Grid item xs={6} className={classes.text}>
+        Human Infection with 2019 Novel Coronovirus Person Under Investigation (PUI) and Case Report
+        Form
+      </Grid>
+    </Grid>
+  );
 }
 
 export default PageHeader;
