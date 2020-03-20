e10e48dfadef7e87fae46c03705ac3817411aa96
Reece Adamson <radamson@mitre.org>
2020-03-20 15:36:55 -0400

    use absolute import

--- src/components/forms/RadioField/RadioField.js
+++ src/components/forms/RadioField/RadioField.js
@@ -5,7 +5,7 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-import { useForm } from '../../FormProvider/FormProvider';
+import { useForm } from 'components/FormProvider';
 
 const useStyles = makeStyles(theme => ({
   formControl: {
