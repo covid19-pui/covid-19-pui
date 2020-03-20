6814ad50278c870f675798b61301c64bfe457e14
Reece Adamson <radamson@mitre.org>
2020-03-20 17:14:28 -0400

    move styles to their own directory

--- src/components/forms/RadioField/RadioField.js
+++ src/components/forms/RadioField/RadioField.js
@@ -1,29 +1,12 @@
 import React, { useState } from 'react';
 import Radio from '@material-ui/core/Radio';
-import { makeStyles } from '@material-ui/core/styles';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import { useForm } from 'components/FormProvider';
 
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  radioGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  formLabel: {
-    fontFamily: theme.typography.primary.fontFamily,
-    color: theme.palette.text.primary
-  }
-}));
+import useStyles from './styles';
 
 function RadioField({ label, formKey, options }) {
   const styles = useStyles();
