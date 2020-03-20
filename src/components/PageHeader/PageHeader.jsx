5a4f323c35aba3fa602c614325669217c3cb7bd1
rdingwell <rob.dingwell@gmail.com>
2020-03-20 11:00:21 -0400

    Page Header (#24)
    * initial cut at page header
    
    * Changes requested for PR.. Fixing spelling and varible naming
    
    * addressing PR comments.  Adding roboto font familiy and addressing styling issues
    
    * Styling updates
    
    Co-authored-by: Noranda Brown <nebrown@mitre.org>

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Image from './covid.png';
+
+const useStyles = makeStyles(theme => ({
+  bg: {
+    backgroundImage: `url(${Image})`,
+    backgroundSize: '100%',
+    backgroundRepeat: 'no-repeat',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    fontFamily: 'Roboto, sans-serif',
+    fontWeight: 300,
+    fontSize: 'calc(12px + 0.9vw)',
+    padding: 'calc(2px + 1vw) 40px',
+    width: '50%',
+    minWidth: '500px'
+  }
+}));
 
 function PageHeader() {
-  return <div>Page header goes here</div>;
+  const styles = useStyles();
+
+  return (
+    <div className={styles.bg}>
+      <div className={styles.text}>
+        Human Infection with 2019 Novel Coronavirus Person Under Investigation (PUI) and Case Report
+        Form
+      </div>
+    </div>
+  );
 }
 
 export default PageHeader;
