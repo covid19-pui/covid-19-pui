f2f4383e2c7576808859d3de738b2afb3d838fe5
Chris Moesel <cmoesel@mitre.org>
2020-03-20 15:58:31 -0400

    Initial DateField and BasicInformationSection

---
+++ src/components/forms/DateField/DateField.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import moment from 'moment';
+import FormControl from '@material-ui/core/FormControl';
+import { KeyboardDatePicker } from '@material-ui/pickers';
+
+import useStyles from './styles';
+import { useForm } from '../../FormProvider/FormProvider';
+
+function DateField({ id, label }) {
+  const styles = useStyles();
+  const { 1: setForm } = useForm();
+  const [selectedDate, setDate] = useState(moment());
+  const handleDateChange = date => {
+    setForm(prevState => {
+      return {
+        ...prevState,
+        [id]: date.format('MM/DD/YYYY')
+      };
+    });
+    setDate(date);
+  };
+  return (
+    <FormControl className={styles.formControl}>
+      <KeyboardDatePicker
+        autoOk
+        variant="inline"
+        inputVariant="outlined"
+        label={label}
+        format="MM/DD/YYYY"
+        value={selectedDate}
+        InputAdornmentProps={{ position: 'end' }}
+        onChange={date => handleDateChange(date)}
+      />
+    </FormControl>
+  );
+}
+
+export default DateField;
