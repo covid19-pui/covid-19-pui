2626da7240863696189b98ed416974a5b2678bb0
Chris Moesel <cmoesel@users.noreply.github.com>
2020-03-24 18:06:05 -0400

    Turn off auto-complete in Chrome (#45)
    * Turn off auto-complete in Chrome
    
    Chrome doesn't honor autocomplete: "off"; you must use autocomplete: "new-password".
    
    * Optimize effect performance in AutocompleteField

--- src/components/forms/AutocompleteField/AutocompleteField.js
+++ src/components/forms/AutocompleteField/AutocompleteField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Autocomplete } from '@material-ui/lab';
 import { Field, useFormikContext } from 'formik';
 import { TextField as MUITextField } from '@material-ui/core';
@@ -8,6 +8,17 @@ export function FormikAutocompleteTextField({ children, options, ...props }) {
   const { onChange, onBlur, value, disabled, name, ...rest } = fieldToTextField(props);
   const { setFieldValue } = useFormikContext();
 
+  // The Autocomplete component appears to override the "autocomplete" attribute, setting it to "off"
+  // no matter how you set the autoComplete property in the component. Chrome, however, ignores
+  // autocomplete: "off" and only really turns off autofill if you set it to autocomplete: "new-password".
+  // This code uses an effect to directly change the autocomplete attribute after the component has
+  // updated.
+  const myRef = React.createRef();
+  useEffect(() => {
+    const input = myRef.current && myRef.current.getElementsByTagName('input')[0];
+    input.autocomplete = 'new-password';
+  }, [myRef]);
+
   return (
     <Autocomplete
       disableClearable
@@ -16,14 +27,7 @@ export function FormikAutocompleteTextField({ children, options, ...props }) {
       options={options}
       getOptionLabel={option => (typeof option === 'string' ? option : option.label)}
       renderInput={params => (
-        <MUITextField
-          {...params}
-          {...rest}
-          name={name}
-          autoComplete="off"
-          variant="outlined"
-          fullWidth
-        />
+        <MUITextField {...params} {...rest} ref={myRef} name={name} variant="outlined" fullWidth />
       )}
       onChange={(event, value) => {
         setFieldValue(name, value);
